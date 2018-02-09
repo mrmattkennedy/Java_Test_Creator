bf1381e9b460c8e1d61eb217783f7a02f76e8d78
Matthew Kennedy <kennemat@arch09.cis.gvsu.edu>
2018-02-09 14:56:06 -0500

    Renamed classes.

---
+++ testClass.java
@@ -0,0 +1,15 @@
+import java.util.Map;
+import java.util.Set;
+
+public class testClass {
+	public static void main(String[] args)
+	{
+		Set<Thread> threadSet = Thread.getAllStackTraces().keySet();
+		Thread[] threadArray = threadSet.toArray(new Thread[threadSet.size()]);
+		
+		for (int i = 0; i < threadArray.length; i++)
+			System.out.println(threadArray[i].getName());
+		
+		
+	}
+}
